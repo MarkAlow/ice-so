6635587f3f1c083d28f5bf693b28b1a4872e6cb0
cod-zone <iox@mail.com>
2020-05-04 13:03:06 -0400

    'commit'

--- client/src/comp/Main/Products.js
+++ client/src/comp/Main/Products.js
@@ -83,7 +83,7 @@ export default function Products(props) {
       </div>
       <Button onClick={() => console.log(typeof iceCreams)}>LOG</Button>
       <grid-container id='iceCreamGrid'>
-        {/* {iceCreams.map((item) => (
+        {iceCreams.map((item) => (
           <grid-item style={{ padding: "1rem" }} key={item.id}>
             <h4 id={item.id} style={{ textAlign: "center", fontWeight: 500 }}>
               <grid-container id='objs'>
@@ -139,7 +139,7 @@ export default function Products(props) {
               </grid-item>
             </div>
           </grid-item>
-        ))} */}
+        ))}
       </grid-container>
     </div>
   );
